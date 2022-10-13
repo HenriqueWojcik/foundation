bb996251399b334020f8de076300663a81c63a0d
Henrique Wojcik <henriquewojcik@brq.com>
2022-10-13 14:14:27 -0300

    init

---
+++ lib/export.dart
@@ -0,0 +1,1 @@
+export 'package:modulo_estrutural/modulo_estrutural.dart';
---
+++ test/foundation_test.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:foundation/foundation.dart';
+
+void main() {}
